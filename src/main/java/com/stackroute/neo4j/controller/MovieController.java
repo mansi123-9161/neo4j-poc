2bbdf3b5fbcc243e12a5d88f5256d6034cb458fc
mansi123-9161 <mansichourasia100@gmail.com>
2019-08-19 21:08:58 +0530

    updated movie controller class

--- src/main/java/com/stackroute/neo4j/controller/MovieController.java
+++ src/main/java/com/stackroute/neo4j/controller/MovieController.java
@@ -1,35 +1,67 @@
-//package com.stackroute.neo4j.controller;
-//
-//import com.stackroute.neo4j.domain.Movie;
-//import com.stackroute.neo4j.service.MovieService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestParam;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Collection;
-//import java.util.Map;
-//
-//@RestController
-//@RequestMapping("/")
-//public class MovieController {
-//    private final MovieService movieService;
-//    @Autowired
-//    public MovieController(MovieService movieService) {
-//        this.movieService = movieService;
-//    }
-//    @GetMapping("/movie")
-//    public Movie findByTitle(@RequestParam String title) {
-//        return movieService.findByTitle(title);
-//    }
-//    @GetMapping("/movies")
-//    public Collection<Movie> findByTitleLike(@RequestParam String title) {
-//        return movieService.findByTitleLike(title);
-//    }
-//    @GetMapping("/graph")
-//    public Map<String, Object> graph(@RequestParam(value = "limit",required = false) Integer limit) {
-//        return movieService.graph(limit == null ? 100 : limit);
-//    }
-//
-//}
+package com.stackroute.neo4j.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("api/v1")
+public class MovieCriticController {
+    private RatingService ratingService;
+    @Autowired
+
+    public MovieCriticController(RatingService ratingService) {
+        this.ratingService = ratingService;
+    }
+    @GetMapping(value = "/critics")
+    public Collection<MovieCritic> getAllMovieCritics() {
+        return ratingService.getAllMovieCritics();
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movies> getAllMovie() {
+        return ratingService.getAllMovies();
+    }
+
+    @PostMapping(value = "/critic")
+    public MovieCritic saveMovieCritic(@RequestBody MovieCritic critic) {
+        System.out.println("****" + critic);
+        return ratingService.saveMovieCritic(critic);
+    }
+
+    @PutMapping(value = "/critic")
+    public MovieCritic updateMovieCritic(@RequestBody MovieCritic critic) {
+        System.out.println("****" + critic);
+        return ratingService.updateMovieCritic(critic);
+    }
+
+    @PostMapping(value = "/movie")
+    public Movies saveMovie(@RequestBody Movies movie) {
+        System.out.println("****" + movie);
+        return ratingService.saveMovie(movie);
+    }
+
+    @PutMapping(value = "/movie")
+    public Movies updateMovie(@RequestBody Movies movie) {
+        System.out.println("****" + movie);
+        return ratingService.updateMovieById(movie);
+    }
+
+    @DeleteMapping(value = "/critic")
+    public MovieCritic deleteCritic(@RequestBody MovieCritic critic) {
+        System.out.println("****" + critic);
+        return ratingService.deleteCriticById(critic);
+    }
+
+    @DeleteMapping(value = "/movie")
+    public Movies deleteMovie(@RequestBody Movies movie) {
+        System.out.println("****" + movie);
+        return ratingService.deleteMovieById(movie);
+    }
+
+    @PostMapping(value = "/critic/set")
+    public MovieCritic setRelation(@RequestParam long criticId, long movieId, int rating) {
+        return ratingService.setRelation(criticId, movieId, rating);
+    }
+}
